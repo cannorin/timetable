99407a3d9397beca0405df2efbc3d0f4c4ec9a51
cannorin <cannorin@users.noreply.github.com>
2024-01-03 18:05:13 +0900

    db: add logging

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,4 +2,9 @@ import "server-only";
 
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+  log:
+    process.env.NODE_ENV === "production"
+      ? ["error"]
+      : ["query", "info", "warn", "error"],
+});
